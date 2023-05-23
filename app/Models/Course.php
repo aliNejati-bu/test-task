fa7995d40c8ac13591f239e90086606d8d656b37
aliNejati-bu <electrocellco@gmail.com>
2023-05-23 15:31:25 +0330

    add Lessons table and model.

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -4,11 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Course extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name','price'];
+    protected $fillable = ['name', 'price'];
+
+
+    /**
+     * @return HasMany
+     */
+    public function lessons(): HasMany
+    {
+        return $this->hasMany(Lesson::class);
+    }
 
 }
---
+++ app/Models/Lesson.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Lesson extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'price', 'course_id'];
+
+    /**
+     * @return BelongsTo
+     */
+    public function course(): BelongsTo
+    {
+        return $this->belongsTo(Course::class);
+    }
+}
